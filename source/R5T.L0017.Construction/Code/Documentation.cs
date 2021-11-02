37cf2ee68492bbe859d1581a9cdd8511a3dc9c46
davidcoats <david@getsafetycone.com>
2021-11-02 16:57:07 -0700

    Initial commit of source-stub.

---
+++ source/R5T.L0017.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0017.Construction
+{
+    /// <summary>
+    /// Construction project for Microsoft.Extensions.Logging related Nuget packages selector..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.L0017/Code/Interfaces/Interface1.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+namespace R5T.L0017
+{
+    public interface Interface1
+    {
+    }
+}
