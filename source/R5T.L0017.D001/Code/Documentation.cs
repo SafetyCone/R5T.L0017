2fc728a13ef87dd189f611e2a00aeb5a204898d4
David Coats <david@getsafetycone.com>
2021-11-03 15:46:25 -0700

    Add ILoggerUnbound service definition for the unbound ILogger<> service.

---
+++ source/R5T.L0017.D001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0017.D001
+{
+    /// <summary>
+    /// Unbound logging service definitions.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.L0017.D001/Code/Services/Definitions/ILoggerUnbound.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+using R5T.T0064;
+
+
+namespace R5T.L0017.D001
+{
+    /// <summary>
+    /// Dummy service definition for the unbound service <see cref="ILogger{TCategoryName}"/>.
+    /// </summary>
+    [ServiceDefinitionMarker]
+    [UnboundGenericServiceDefinitionMarker(typeof(ILogger<>))]
+    public interface ILoggerUnbound : IServiceDefinition, IUnboundGenericServiceDefinition
+    {
+    }
+}
